809f0473294a3b215699830455205b0c44247f01
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-21 14:30:00 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Post } from "../Post"
+import { Header } from "./components/Header"
+
+function App() {
+  return (
+
+    <div>
+      <Header />
+      <Post 
+        author='Jessica Guedes'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus inventore quo laudantium nulla officiis, dicta maiores voluptatibus qui a harum, et dignissimos molestias sunt. Quibusdam et quam adipisci. Ad, quaerat!'
+      />
+
+      <Post 
+        author='Leticia Melo'
+        content='Um novo post muito legal'
+      />
+    </div>
+  )
+}
+export default App
