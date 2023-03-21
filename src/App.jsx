f0639fc73b73584017066d84f89edda774a01629
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-21 17:13:44 -0400

    component Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { Post } from "../Post"
 import { Header } from "./components/Header"
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
 
     <div>
       <Header />
-      <Post 
-        author='Jessica Guedes'
-        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus inventore quo laudantium nulla officiis, dicta maiores voluptatibus qui a harum, et dignissimos molestias sunt. Quibusdam et quam adipisci. Ad, quaerat!'
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+        <Post 
+          author='Jessica Guedes'
+          content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus inventore quo laudantium nulla officiis, dicta maiores voluptatibus qui a harum, et dignissimos molestias sunt. Quibusdam et quam adipisci. Ad, quaerat!'
+        />
 
-      <Post 
-        author='Leticia Melo'
-        content='Um novo post muito legal'
-      />
+        <Post 
+          author='Leticia Melo'
+          content='Um novo post muito legal'
+        />
+        </main>
+      </div>
     </div>
   )
 }
