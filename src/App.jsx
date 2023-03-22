0458b12854f6d4aee8e163ce50b47cb26d51d01b
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-22 10:34:48 -0400

    component post - preparando a estrutura html

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Post } from "../Post"
+import { Post } from "./components/Post"
 import { Header } from "./components/Header"
 
 import styles from './App.module.css';
@@ -16,15 +16,9 @@ function App() {
         <Sidebar />
         
         <main>
-        <Post 
-          author='Jessica Guedes'
-          content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus inventore quo laudantium nulla officiis, dicta maiores voluptatibus qui a harum, et dignissimos molestias sunt. Quibusdam et quam adipisci. Ad, quaerat!'
-        />
+        <Post />
 
-        <Post 
-          author='Leticia Melo'
-          content='Um novo post muito legal'
-        />
+        
         </main>
       </div>
     </div>
