977a716e6d81b0cace4ec22aa90f19a7e7f727cd
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-22 09:51:49 -0400

    finalizando sidebar e add o icone

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine} from '@phosphor-icons/react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -9,12 +11,15 @@ export function Sidebar(){
       />
 
       <div className={styles.profile}>
-        <strong>Jessica Guedes</strong>
+        <img className={styles.avatar} src="https://github.com/jessica-guedes.png"  />
+
+        <strong>Jéssica Guedes</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: true
-    }
+    },
+    port: 3000,
   }
 })
