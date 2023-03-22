28299395ef20ecaa19632d05aaaddce79a1667ec
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-22 11:34:27 -0400

    criação da estrutura do formulário e estilização

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,7 @@ export function Post (){
         <div className={styles.author}>
           <img className={styles.avatar} src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
-            <strong>Deigo Fernandes</strong>
+            <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
           </div>
         </div>
@@ -21,20 +21,26 @@ export function Post (){
 
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
         <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-
         <p>
-        <a href="">#novoprojeto</a>{' '}
+          <a href="">#novoprojeto</a>{' '}
           <a href="">#nlw</a> {' '}
           <a href="">#rocketseat</a>{' '}
         </p>
- 
-
       </div>
+
+      <form className={styles.comentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
