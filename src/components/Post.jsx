f2a8174386bfd33fc5296939e63a1063a3ed06f5
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-22 18:35:09 -0400

    useState para adicionar mais comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,18 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }){
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
+
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
     locale: ptBR
   })
@@ -15,6 +22,12 @@ export function Post({ author, publishedAt, content }){
     addSuffix: true,
     
   })
+
+  function handleCreateNewComment(){
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1]);
+  }
 
   return(
     <article className={styles.post}>
@@ -33,16 +46,16 @@ export function Post({ author, publishedAt, content }){
       </header>
 
       <div className={styles.content}>
-      {content.map(line => {
-          if (line.type === 'paragraph'){
-            return <p>{line.content}</p>;
-          }else if (line.type === 'link'){
-            return <p><a href="#">{line.content}</a></p>
-          }
-        })}
+        {content.map(line => {
+            if (line.type === 'paragraph'){
+              return <p>{line.content}</p>;
+            }else if (line.type === 'link'){
+              return <p><a href="#">{line.content}</a></p>
+            }
+          })}
       </div>
 
-      <form className={styles.comentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.comentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea 
@@ -55,9 +68,9 @@ export function Post({ author, publishedAt, content }){
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
