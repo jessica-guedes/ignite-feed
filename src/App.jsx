b16b261de455d340446a31a2c10d92eca4be9d89
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-22 16:30:14 -0400

    iterações no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from "./components/Sidebar";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉 jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-03-22 16:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉 jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-03-20 16:00:00'),
+  },
+];
+
 function App() {
   return (
 
@@ -16,14 +47,20 @@ function App() {
         <Sidebar />
         
         <main>
-        <Post />
-        <Post />
-
-
-        
+          {posts.map(post => {
+            return (
+            <Post  
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            
+            />)
+          })}
         </main>
       </div>
     </div>
   )
 }
+
 export default App
+
