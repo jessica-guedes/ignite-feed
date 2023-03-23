bd6fd88f1fdb93abcd33e922643def0d5f685d22
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-23 10:45:04 -0400

    inserindo comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment({content}) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https:github.com/jakeliny.png" alt="" />
@@ -25,7 +25,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom Diego, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
